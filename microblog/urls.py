9a96a549bc6b42cac46e58d2647b98381051d3a8
Kenneth Love <kenneth@brack3t.com>
2012-12-10 22:28:40 +0000

    blank 500.html, simple index.html. added url for /

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
