360f5ff091a59fd43654f4eb970d221130c03bd3
Kenneth Love <kenneth@brack3t.com>
2013-02-11 20:38:27 +0000

    moved MEDIA_ROOT and STATIC_ROOT one level closer

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -44,7 +44,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = root("..", "..", "uploads")
+MEDIA_ROOT = root("..", "uploads")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -55,7 +55,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = root("..", "..", "static")
+STATIC_ROOT = root("..", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
